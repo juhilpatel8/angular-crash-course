2496fde6d4440241568dccd9844b77a0ce8496d3
Michael Zech <michael@Michaels-Mini.fritz.box>
2019-04-06 13:34:08 +0200

    Improved the second exercise of section 03.

--- sec03-binding/src/app/text-box/text-box.component.ts
+++ sec03-binding/src/app/text-box/text-box.component.ts
@@ -14,21 +14,25 @@ export class TextBoxComponent implements OnInit {
   }
 
   handleKeyUp($event) {
-    this.sReturn = this.correctCase($event.target.value);
+    this.sReturn = this.translateToTitleCase($event.target.value);
   }
 
-  correctCase(str) {
-    const correctWord = (sWord) => {
+  // Into unmerciful the entreating stronger to of word guessing.
+  // the OLD MAN aND THE sEa  =>  The Old Man and the Sea 
+  translateToTitleCase(str) {
+    const translateWord = (sWord) => {
       return sWord.slice(0, 1).toUpperCase() + sWord.slice(1).toLowerCase();
     }
 
     const parts = str.split(" ");
    
-    parts[0] = correctWord(parts[0]);
+    parts[0] = translateWord(parts[0]);
 
     for (let i = 1; i < parts.length; i++) {
-      if (!["of", "and", "the"].includes(parts[i].toLowerCase())) {
-        parts[i] = correctWord(parts[i]);;
+      if (!["of", "and", "the", "to"].includes(parts[i].toLowerCase())) {
+        parts[i] = translateWord(parts[i]);;
+      } else {
+        parts[i] = parts[i].toLowerCase();  // Make sure is's the correct case, when the sentence (or parts of it) is given in uppercase.
       }
     }
 
